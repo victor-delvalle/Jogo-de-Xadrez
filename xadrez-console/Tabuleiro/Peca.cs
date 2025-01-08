f9e3a097e548de20e7ad0325573b51582211130b
Victor <vitinhodelvalle@outlook.com>
2025-01-08 18:12:10 -0300

    implementanto as restricoes de movimento

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -26,6 +26,28 @@ namespace tabuleiro
         {
             qtedMovimentos++;
         }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
         public abstract bool[,] movimentosPossiveis();
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -47,6 +47,7 @@ namespace xadrez_console
                     }
 
                     imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
                 }
 
                 Console.WriteLine();
